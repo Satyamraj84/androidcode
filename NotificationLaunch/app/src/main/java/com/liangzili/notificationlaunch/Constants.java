d246bf1ffbd1c149e578f32f3490af6021e10665
liangzili <lzl0120@gmail.com>
2015-08-06 17:34:16 +0800

    增加NotificationLaunch

---
+++ NotificationLaunch/app/src/main/java/com/liangzili/notificationlaunch/Constants.java
@@ -0,0 +1,8 @@
+package com.liangzili.notificationlaunch;
+
+/**
+ * Created by liangzili on 15/8/3.
+ */
+public class Constants {
+    public static final String EXTRA_BUNDLE = "launchBundle";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#### NotificationLaunch 
+Android实现点击通知栏后，先启动应用再打开目标Activity的一个小demo
